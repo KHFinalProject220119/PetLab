a67fe136f2ea3396a8cd073a3d2530ad805b40cb
Spring <pringpringspring@gmail.com>
2022-07-31 04:27:21 +0900

    [220731] 백업 (브랜드오류+savedirectory하기)

--- src/main/java/com/kh/petlab/store/model/dao/StoreDao.java
+++ src/main/java/com/kh/petlab/store/model/dao/StoreDao.java
@@ -1,6 +1,14 @@
 package com.kh.petlab.store.model.dao;
 
-import com.kh.petlab.store.model.dto.*;
-public interface StoreDao {
+import org.apache.ibatis.annotations.Mapper;
 
+import com.kh.petlab.member.model.dto.Attachment;
+import com.kh.petlab.store.model.dto.ProductEntity;
+
+@Mapper
+public interface StoreDao {
+	/* int insertProduct(ProductEntity product); */
+	int insertProduct(ProductEntity product);
+	
+	int insertAttachment(Attachment attach);
 }
--- src/main/java/com/kh/petlab/store/model/service/StoreService.java
+++ src/main/java/com/kh/petlab/store/model/service/StoreService.java
@@ -1,5 +1,9 @@
 package com.kh.petlab.store.model.service;
 
+import com.kh.petlab.store.model.dto.ProductEntity;
+
 public interface StoreService {
 
+	/* public void insertProduct(ProductEntity product); */
+	int insertProduct(ProductEntity product);
 }
