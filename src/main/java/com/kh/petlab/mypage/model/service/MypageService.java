d141cafc9a791a02b187697d6992f33747adaad4
js <js@DESKTOP-G9UT33J>
2022-08-18 00:37:51 +0900

    서비스 1차

--- src/main/java/com/kh/petlab/mypage/model/service/MypageService.java
+++ src/main/java/com/kh/petlab/mypage/model/service/MypageService.java
@@ -7,16 +7,20 @@ import com.kh.petlab.member.model.dto.Member;
 public interface MypageService {
 	
 	String ROLE_USER = "ROLE_USER";
+	String ROLE_ADMIN = "ROLE_ADMIN";
 	String ROLE_HOSPITAL = "ROLE_HOSPITAL";
 	String ROLE_MALL = "ROLE_MALL";
-	
+	String ROLE_COUNSELOR = "ROLE_COUNSELOR";
 	Member selectOneMember(String memberId);
-
+	Member selectOneMemberByNickname(String nickname);
+	Member selectOneMemberByEmail(String email);
+	
 	int updateMember(Member member);
 
 	List<Member> selectMemberList();
 
 	int updateMemberRole(String memberId, List<String> authorities);
+
 	
 
 }
