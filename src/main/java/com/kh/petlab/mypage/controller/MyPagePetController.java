f4be50502cc8cfc29bc3ed0d61328a45e18a9b99
js <js@DESKTOP-G9UT33J>
2022-08-18 00:37:12 +0900

    와 컨트롤러 한번 잘못건드렸다가 다 날려먹을뻔 했다

---
+++ src/main/java/com/kh/petlab/mypage/controller/MyPagePetController.java
@@ -0,0 +1,18 @@
+package com.kh.petlab.mypage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/mypage")
+@Slf4j
+public class MyPagePetController {
+
+	@GetMapping("/myPetHome")
+	public void myPetHome() {
+		
+	}
+}
