f361ca1d7eff4026bb39db706c30f0f0a1f39d9f
skygirl20 <parkmin100@naver.com>
2022-08-19 02:17:25 +0900

    커뮤니티/커뮤니티 entity

--- src/main/java/com/kh/petlab/community/model/dto/Community.java
+++ src/main/java/com/kh/petlab/community/model/dto/Community.java
@@ -1,5 +1,31 @@
 package com.kh.petlab.community.model.dto;
 
-public class Community {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.kh.petlab.member.model.dto.Attachment;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Community{
+	
+	private int freebNo;
+	private String freebTitle;
+	private String content;
+	private int readCount;
+	private LocalDateTime regDate;
+	private int likeCount;
 }
